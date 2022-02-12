ed68cab04a6d404635767c950856e2c34971b771
prathap <prathapp642@gmail.com>
2022-02-12 23:09:29 +0530

    working check point dated 11-2-2022

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,13 @@ import Header from "./components/Header"
 
 
 function App() {
+ 
   return <>
-<Header name='Feedback UI'/>
+
+<Header name='Feedback UI' bgColor="rgba(0,0,0,0.4)" textColor="#ff6a95" />
+<div className="container">
+  <h1>My App</h1>
+</div>
   </>
     
   
